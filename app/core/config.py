7df3512633afd7a5f0187e2c6e3729792f2e421f
Sithija_Imesh <156312972+Sithijaimesh@users.noreply.github.com>
2026-03-04 21:06:56 +0530

    Done the backend part also in the delete and edit medicine

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,14 @@
-# app/core/config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    encryption_key: str 
+    ENCRYPTION_KEY: str
 
-    class Config:
-        env_file = ".env"
-        extra = "ignore" 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+
 settings = Settings()
